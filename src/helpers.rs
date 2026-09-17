0650b994127b0bd5887c0e8e770cc0e801529991
null <60427892+null8626@users.noreply.github.com>
2026-09-17 22:05:35 +0700

    refactor[helpers]: make parse_content alloc free

--- src/helpers.rs
+++ src/helpers.rs
@@ -3,7 +3,7 @@ use std::collections::BTreeMap;
 pub(crate) fn mac_to_u64(mac: &str) -> Option<u64> {
     let hex: String = mac.split(|c| c == ':').filter(|s| !s.is_empty()).collect();
     let padded_hex = match hex.len() {
-        6 => format!("{}000000", hex),
+        6 => format!("{hex}000000"),
         12 => hex,
         _ => return None,
     };
@@ -11,7 +11,7 @@ pub(crate) fn mac_to_u64(mac: &str) -> Option<u64> {
     u64::from_str_radix(&padded_hex, 16).ok()
 }
 
-pub(crate) fn mask_mac(mac: u64, cidr: u8) -> u64 {
+pub(crate) const fn mask_mac(mac: u64, cidr: u8) -> u64 {
     let mask = match cidr {
         24 => 0xFFFFFF000000,
         28 => 0xFFFFFFF00000,
@@ -25,26 +25,32 @@ pub(crate) fn parse_content(source: &str) -> BTreeMap<(u64, u8), String> {
     let mut data = BTreeMap::<(u64, u8), String>::new();
 
     for line in source.lines() {
-        let current_line = line.replace("\t\t", "\t");
-        let fields = current_line.split('\t').collect::<Vec<&str>>();
-
-        if fields[0].starts_with("#") || line.is_empty() {
+        let Some(tab_index) = line.find('\t') else {
             continue;
+        };
+
+        let (Some(mac), Some(mut manuf)) = (line.get(0..tab_index), line.get(tab_index + 1..)) else {
+            continue;
+        };
+
+        if matches!(manuf.as_bytes().first(), Some(b'\t')) {
+            manuf = &manuf[1..];
         }
 
-        let mac = fields[0];
-        let manuf = fields[1].to_string();
         if let Some((mac_prefix, cidr)) = mac.split_once('/') {
             if let Some(mac_val) = mac_to_u64(mac_prefix) {
                 let cidr = cidr.parse::<u8>().unwrap_or_default();
+
                 if cidr == 28 || cidr == 36 {
-                    data.insert((mask_mac(mac_val, cidr), cidr), manuf.clone());
+                    data.insert((mask_mac(mac_val, cidr), cidr), manuf.into());
+
                     continue;
                 }
             }
         } else if let Some(mac_val) = mac_to_u64(mac) {
             let cidr = 24;
-            data.insert((mask_mac(mac_val, cidr), cidr), manuf.clone());
+
+            data.insert((mask_mac(mac_val, cidr), cidr), manuf.into());
         }
     }
 
